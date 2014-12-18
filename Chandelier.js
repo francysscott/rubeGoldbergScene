08e5ff683b06569dd9462228ef4f97f44b6266d6
francysscott <francys.scott@gmail.com>
2014-12-18 18:43:38 -0500

    Refactored Chandelier
    Converted helper functions into local functions within
    createChandelier() as Scott suggested in his notes on our alpha version
    of the project.

--- Chandelier.js
+++ Chandelier.js
@@ -18,8 +18,8 @@
 	Francys Scott and Susie Carovillano
 	December 2014
 	
-	Builds a chandelier based on variables including number of branches, material colors,
-	size and shape (of glass globe).
+	Builds a chandelier based on variables including number of branches (minimum 2, 
+	maximum 6), material colors, size and shape of glass globe.  
 	
 	Chandelier origin is the center of the top plane of the mount from which hangs 
 	the rest of the chandelier.  
@@ -33,14 +33,15 @@
 		height to specify the chandelier.position.set.y.
 	*/
 	
-/*
-	
+/**
 	createChandelier(inputHeight, numBranches, isLipped, inputGlassColor, inputMetalColor)
 	Creates a chandelier based on user preference to height and width of object and 
 	user-specified colors/textures for the materials.  Note that the height is always 
 	even, or gets changed to an even integer, because this affects the ability to 
 	calculate the sin wave for the branches.  (An odd integer breaks the equation.)
-	int height the overall height of the chandelier including mount and chain
+	int height the overall height of the chandelier including mount and chain.  Note that 
+		if this number is even, the final height of the object is slightly adjusted in 
+		order to prevent a fatal error when calculating the sin curve for the arm.
 	int numBranches the number of lights and branches the user wants.  User should set 
 		2-6 branches.  If user sets more, defaults to 6; if fewer, defaults to 2.
 	bool isLipped whether the glass globes are lipped or spherical
@@ -51,6 +52,172 @@
 */               
 function createChandelier(inputHeight, numBranches, isLipped,
 							inputGlassColor, inputMetalColor, inputMetalShininess){
+							
+	/** 
+	getSimpleGlobePoints()
+	Returns an array of values for the points to make a simple globe with open top.
+	The globe is used in making each branch of a chandelier
+	*/
+	function getSimpleGlobePoints() {
+		var points = [ [0, 0, 0],    //silhouette of globe
+						[0.4, 0, 0],
+						[0.5, 0, 0.05],
+						[0.58, 0, 0.12],
+						[0.65, 0, 0.2],
+						[0.69, 0, 0.3],
+						[0.72, 0, .4],
+						[0.75, 0, .5],
+						[0.74, 0, .6],
+						[0.7, 0, .8],
+						[0.6, 0, .9],
+						[0.5, 0, .95],
+						[0.35, 0, .95],
+						[.4, 0, .9],
+						[.55, 0, .8],
+						[.6, 0, .55],
+						[.5, 0, .3],
+						[.4, 0, .2],
+						[.3, 0, .15],
+						[0, 0, .14] ];  // closes off the bottom of the lathe
+		return points;
+	}
+
+	/** 
+		getLippedGlobePoints()
+		Returns the points for the lipped globe with open top.  Used to create the glass globes
+		for the chandelier.
+	*/
+	function getLippedGlobePoints() {
+		var points = [ [0, 0, 0],    //silhouette of globe
+						[.4, 0, 0],
+						[.5, 0, .05],
+						[.58, 0, .12],
+						[.65, 0, .2],
+						[.69, 0, .3],
+						[.72, 0, .4],
+						[.75, 0, .5],
+						[.77, 0, .6],
+						[.8, 0, .7],
+						[.82, 0, .75],
+						[.89, 0, .9],
+						[.9, 0, .92],
+						[.95, 0, .97],
+						[.96, 0, .98],
+						[.95, 0, .99],
+						[.93, 0, .98],
+						[.9, 0, .97],
+						[.85, 0, .95],
+						[.8, 0, .9],
+						[.7, 0, .8],
+						[.6, 0, .55],
+						[.5, 0, .3],
+						[.4, 0, .2],
+						[.3, 0, .15],
+						[0, 0, .14] ];  // closes off the bottom of the lathe
+		return points;
+	}										
+							
+	/**
+	createGlobeGeom(globeHeight, globeRad, isLipped)
+	Creates a geometry for a globe with open top for the chandelier.  The shape of said 
+	globe is determined by the boolean isLipped.  
+	*/
+	function createGlobeGeom(globeHeight, globeRad, isLipped)  {
+		// Type of chandelier is based on whether user wants simple round or fluted style
+		var points;
+		if (isLipped) 
+			points = getLippedGlobePoints();
+		else points = getSimpleGlobePoints();
+					
+		var alt = [];
+		var size = globeHeight;
+		for( var i=0; i<points.length; i++ ) { // Puts points in Vector3 form to use in lathe
+			var p = new THREE.Vector3();
+			p.x = globeRad * points[i][0];	// scale width of globe to match width param
+			p.z = size * points[i][2];		// scale length of globe to match height param
+			p.y = points[i][1];;
+			alt.push(p);
+		}
+		var geom = new THREE.LatheGeometry( alt, 18 ); 
+		// 18 segments make it look like an generally rounded globe without using 
+		// too much processor resources.
+	
+		return geom;
+	}						
+							
+	/**
+	Partially derived from Threejs website, this returns the geometry of the
+	curved arm of the branch.
+	*/
+	function createBranchGeom(height, width)  {
+
+		var CustomSinCurve = THREE.Curve.create(
+			function ( scale ) { //custom curve constructor
+				// if no scale is specified, default scale is 1:1.
+				this.scale = (scale === undefined) ? 1 : scale;
+			},
+	
+			function ( t ) { //getPoint: t is between 0-1
+				var tx = t * 3 - 1.5,
+					ty = Math.sin( 2 * Math.PI * t ),
+					tz = 0;
+				return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
+			}
+		);
+
+		// Build curve and return the values of the 2D curve.
+		var path = new CustomSinCurve( width/4 );
+		var geometry = new THREE.TubeGeometry(
+			path,  				//path
+			width * 1.5,    	//segments
+			width * 0.0425,     //radius of tube
+			width/2,     		//radiusSegments
+			false  				//not closed
+		);
+		return geometry;
+	}
+												
+	/**
+	Creates whole branch including globe and arm.  Returns the branch object mesh.
+	*/
+	function createBranch(height, width, glassTexture, branchTexture, isLipped)  {
+		var branch = new THREE.Object3D();
+
+		// glass globe in which is a light                               		
+		var globeGeom = createGlobeGeom(height/3, width/4, isLipped);
+		var globe1 = new THREE.Mesh(globeGeom,glassTexture);
+	
+		// wood base for each globe, always lipped
+		var baseGeom = createGlobeGeom(height/8, width/5, true);
+		var base1 = new THREE.Mesh(baseGeom,branchTexture);
+	
+		// arm that attaches globe to base and ceiling
+		var armGeom = createBranchGeom(height, width);
+		var arm1 = new THREE.Mesh(armGeom,branchTexture);
+	
+		// position arm
+		arm1.rotateY(-Math.PI/2);
+		arm1.rotateZ(-Math.PI/6);
+		arm1.position.set(0,height/6,width/6);
+	
+		// position globe
+		globe1.rotateX(-Math.PI/2);
+		globe1.position.set(0,0,width/2);
+	
+		//position base
+		base1.rotateX(-Math.PI/2);
+		base1.position.set(0,-height/20,width/2);
+	
+		branch.add(globe1);
+		branch.add(arm1);
+		branch.add(base1);
+	
+		return branch;
+	}						
+							
+	// Local functions finished.
+	// Begin code to create chandelier.  
+	
 	var chandelier = new THREE.Object3D();	// frame in which all pieces are placed
 	
 	// Basic materials for testing, amber glass and brass metal fixture
@@ -66,7 +233,7 @@ function createChandelier(inputHeight, numBranches, isLipped,
     										 ambient: inputMetalColor,
                                              shininess: inputMetalShininess,
                                     		});
-                                    		                                    		
+    // Make sure the user's input values make sense for this object.  Otherwise fix them.                              		                                    		
 	// make sure the number of branches is within reason, else limit it.
 	var actualNumBranches = numBranches;
 	if (actualNumBranches > 6) {
@@ -82,7 +249,8 @@ function createChandelier(inputHeight, numBranches, isLipped,
 		height--;
 	}
 	var width = height;	// keep relationship the same between width and height
-	var degree = 2*Math.PI/actualNumBranches;  // angle around the chandelier
+						// so the proportion stays reasonable.
+	var degree = 2*Math.PI/actualNumBranches;  // angle around the chandelier of each branch
 	
 	// Create basic branch
 	var branch1 = createBranch(height, width, glassM, metalM, isLipped);
@@ -117,170 +285,9 @@ function createChandelier(inputHeight, numBranches, isLipped,
     chandelier.add(spotLight);
 
 	return chandelier;
+
+	
 }   
 
 
-/**
-	Creates whole branch including globe and arm.
-*/
-function createBranch(height, width, glassTexture, branchTexture, isLipped)  {
-	var branch = new THREE.Object3D();
 
-    // glass globe in which is a light                               		
-	var globeGeom = createGlobeGeom(height/3, width/4, isLipped);
-	var globe1 = new THREE.Mesh(globeGeom,glassTexture);
-	
-	// wood base for each globe, always lipped
-	var baseGeom = createGlobeGeom(height/8, width/5, true);
-	var base1 = new THREE.Mesh(baseGeom,branchTexture);
-	
-	// arm that attaches globe to base and ceiling
-	var armGeom = createBranchGeom(height, width);
-	var arm1 = new THREE.Mesh(armGeom,branchTexture);
-	
-	// position arm
-	arm1.rotateY(-Math.PI/2);
-	arm1.rotateZ(-Math.PI/6);
-	arm1.position.set(0,height/6,width/6);
-	
-	// position globe
-	globe1.rotateX(-Math.PI/2);
-	globe1.position.set(0,0,width/2);
-    
-	//position base
-	base1.rotateX(-Math.PI/2);
-	base1.position.set(0,-height/20,width/2);
-	
-	branch.add(globe1);
-	branch.add(arm1);
-	branch.add(base1);
-	
-	return branch;
-}
-
-/**
-	Partially derived from Threejs website, this returns the geometry of the
-	curved arm of the branch.
-*/
-function createBranchGeom(height, width)  {
-
-	var CustomSinCurve = THREE.Curve.create(
-		function ( scale ) { //custom curve constructor
-			this.scale = (scale === undefined) ? 1 : scale;
-		},
-	
-		function ( t ) { //getPoint: t is between 0-1
-			var tx = t * 3 - 1.5,
-				ty = Math.sin( 2 * Math.PI * t ),
-				tz = 0;
-		
-			return new THREE.Vector3(tx, ty, tz).multiplyScalar(this.scale);
-		}
-	);
-
-	var path = new CustomSinCurve( width/4 );
-
-	var geometry = new THREE.TubeGeometry(
-		path,  				//path
-		width * 1.5,    	//segments
-		width * 0.0425,     //radius of tube
-		width/2,     		//radiusSegments
-		false  				//not closed
-	);
-	
-	return geometry;
-}
-
-
-/**
-	createGlobeGeom(globeHeight, globeRad, isLipped)
-	Creates a globe with open top for the chandelier.
-*/
-function createGlobeGeom(globeHeight, globeRad, isLipped)  {
-	
-	// Type of chandelier is based on whether user wants simple or fluted style
-	var points;
-	if (isLipped) 
-		points = getLippedGlobePoints();
-	else points = getSimpleGlobePoints();
-                   	
-    var alt = [];
-    var size = globeHeight;
-    for( var i=0; i<points.length; i++ ) { // Puts points in Vector3 form to use in lathe
-        var p = new THREE.Vector3();
-        p.x = globeRad * points[i][0];	// scale width of globe to match width param
-        p.z = size * points[i][2];		// scale length of globe to match height param
-        p.y = points[i][1];;
-        alt.push(p);
-    }
-    
-    var geom = new THREE.LatheGeometry( alt, 18 ); 
-    // 18 segments make it look like an generally rounded globe without using 
-    // too much processor resources.
-    
-	return geom;
-}
-
-
-/** 
-	getSimpleGlobePoints()
-	Returns the points for the simple globe with open top.
-*/
-function getSimpleGlobePoints() {
-	var points = [ [0, 0, 0],    //silhouette of globe
-					[0.4, 0, 0],
-					[0.5, 0, 0.05],
-					[0.58, 0, 0.12],
-				   	[0.65, 0, 0.2],
-				   	[0.69, 0, 0.3],
-                   	[0.72, 0, .4],
-                   	[0.75, 0, .5],
-                   	[0.74, 0, .6],
-                   	[0.7, 0, .8],
-                   	[0.6, 0, .9],
-                   	[0.5, 0, .95],
-                   	[0.35, 0, .95],
-                   	[.4, 0, .9],
-                   	[.55, 0, .8],
-                   	[.6, 0, .55],
-                   	[.5, 0, .3],
-                   	[.4, 0, .2],
-                   	[.3, 0, .15],
-                   	[0, 0, .14] ];  // closes off the bottom of the lathe
-    return points;
-}
-
-/** 
-	getLippedGlobePoints()
-	Returns the points for the lipped globe with open top.
-*/
-function getLippedGlobePoints() {
-	var points = [ [0, 0, 0],    //silhouette of globe
-					[.4, 0, 0],
-					[.5, 0, .05],
-					[.58, 0, .12],
-				   	[.65, 0, .2],
-				   	[.69, 0, .3],
-                   	[.72, 0, .4],
-                   	[.75, 0, .5],
-                   	[.77, 0, .6],
-                   	[.8, 0, .7],
-                   	[.82, 0, .75],
-                   	[.89, 0, .9],
-                   	[.9, 0, .92],
-                   	[.95, 0, .97],
-                   	[.96, 0, .98],
-                   	[.95, 0, .99],
-                   	[.93, 0, .98],
-                   	[.9, 0, .97],
-                   	[.85, 0, .95],
-                   	[.8, 0, .9],
-                   	[.7, 0, .8],
-                   	[.6, 0, .55],
-                   	[.5, 0, .3],
-                   	[.4, 0, .2],
-                   	[.3, 0, .15],
-                   	[0, 0, .14] ];  // closes off the bottom of the lathe
-    return points;
-}
-          
