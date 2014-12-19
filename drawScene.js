7aa8e2d4e15b9401212c0ab534d809da481a7b29
francysscott <francys.scott@gmail.com>
2014-12-19 17:35:08 -0500

    Next steps of curtain

--- drawScene.js
+++ drawScene.js
@@ -255,32 +255,52 @@ function drawScene(sceneParams) {
 	
 	// Add the curtain.
 	// cloth material
-// 	var clothTexture = THREE.ImageUtils.loadTexture( sceneParams.umbrellaTexture );
-// 	clothTexture.wrapS = clothTexture.wrapT = THREE.RepeatWrapping;
-// 	clothTexture.anisotropy = 16;
-// 
-// 	var clothMaterial = new THREE.MeshPhongMaterial( { alphaTest: 0.5, ambient: 0xffffff, color: 0xffffff, specular: 0x030303, emissive: 0x111111, shiness: 10, map: clothTexture, side: THREE.DoubleSide } );
-// 
-// 	// cloth geometry
-// 	clothGeometry = new THREE.ParametricGeometry( clothFunction, cloth.w, cloth.h );
-// 	clothGeometry.dynamic = true;
-// 	clothGeometry.computeFaceNormals();
-// 
-// 	var uniforms = { texture:  { type: "t", value: clothTexture } };
-// 	var vertexShader = document.getElementById( 'vertexShaderDepth' ).textContent;
-// 	var fragmentShader = document.getElementById( 'fragmentShaderDepth' ).textContent;
-// 
-// 	// cloth mesh
-// 
-// 	object = new THREE.Mesh( clothGeometry, clothMaterial );
-// 	object.position.set( 0, 0, 0 );
-// 	object.castShadow = true;
-// 	object.receiveShadow = true;
-// 	scene.add( object );
-// 
-// 	object.customDepthMaterial = new THREE.ShaderMaterial( { uniforms: uniforms, vertexShader: vertexShader, fragmentShader: fragmentShader } );
+	var clothTexture = THREE.ImageUtils.loadTexture( sceneParams.umbrellaTexture );
+	clothTexture.wrapS = clothTexture.wrapT = THREE.RepeatWrapping;
+	clothTexture.anisotropy = 16;
 
-			
+	var clothMaterial = new THREE.MeshPhongMaterial( { alphaTest: 0.5, 
+													ambient: 0xffffff, 
+													color: 0xffffff, 
+													specular: 0x030303, 
+													emissive: 0x111111, 
+													shininess: 10, 
+													map: clothTexture, 
+													side: THREE.DoubleSide } );
+	// cloth geometry
+	clothGeometry = new THREE.ParametricGeometry( clothFunction, cloth.w, cloth.h );
+	clothGeometry.dynamic = true;
+	clothGeometry.computeFaceNormals();
+
+	var uniforms = { texture:  { type: "t", value: clothTexture } };
+	var vertexShader = document.getElementById( 'vertexShaderDepth' ).textContent;
+	var fragmentShader = document.getElementById( 'fragmentShaderDepth' ).textContent;
+
+	// cloth mesh
+	objectC = new THREE.Mesh( clothGeometry, clothMaterial );
+	objectC.position.set( -135, 0, -100 );
+	objectC.scale.set(0.25, 0.4, 0.4);
+	objectC.rotateY(Math.PI/2);
+	scene.add( objectC );
+
+	objectC.customDepthMaterial = new THREE.ShaderMaterial( { uniforms: uniforms, vertexShader: vertexShader, fragmentShader: fragmentShader } );
+
+	//Curtain rods for the curtains
+	// poles
+	var poleGeo = new THREE.BoxGeometry( 20, 3, 3 );
+	var poleMat = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0x111111, shiness: 30 } );
+
+	var mesh = new THREE.Mesh( poleGeo, poleMat );
+	mesh.position.set( -135, 0, -100 );
+	scene.add( mesh );
+
+	var mesh = new THREE.Mesh( poleGeo, poleMat );
+	mesh.position.set( -135, 0, -100 );
+	scene.add( mesh );
+
+	var mesh = new THREE.Mesh( new THREE.BoxGeometry( 2, 2, 80 ), poleMat );
+	mesh.position.set( -135, 150, -100 );
+	scene.add( mesh );
 	
 	
     TW.render();
